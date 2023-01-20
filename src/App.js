111dbde128fff9bdb8ad3586d4f10e37939196cb
BenvindoDev <allinybenvindo@gmail.com>
2023-01-20 11:03:36 -0300

    atualizando..

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './components/HelloWorld';
 
 function App() {
   return (
     <div className="App">
       <h1>Olá React!</h1>
       <p>Meu primeiro app utilizando React :)</p>
+      <HelloWorld />
+      
     </div>
   );
 }
