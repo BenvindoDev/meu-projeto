c54e933beb968792efe49d17a0f68acb30e01618
Alliny Machado <alliny.machado@globalhitss.com.br>
2023-01-24 11:21:56 -0300

    Css Modules

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
+import Frase from './components/Frase';
+
+
 function App() {
 
   const nome = "Caleb"
 
   return (
     <div className="App">
+      <h1>Testando CSS</h1>
+      <Frase />
+      <Frase />
       <SayMyName nome="Leonardo" />
       <SayMyName nome="Alliny" />
       <SayMyName nome={nome} />
